5c8e7f701f75463158ce9a334e8da08542716649
Brandon Tran <brandon.tran@prescryptive.com>
2024-06-28 16:08:09 -0700

    WIP

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,24 +1,20 @@
 ﻿using InstagramCloneAPI.Dtos;
+using InstagramCloneAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BCrypt.Net;
 
 namespace InstagramCloneAPI.Services
 {
-    public class AuthService : IAuthService
+    public class AuthService(ApplicationDbContext context) : IAuthService
     {
         public async Task<(bool IsSuccess, string Token, string ErrorMessage)> LoginAsync(LoginDto loginRequest)
         {
-            // Implement your authentication logic here
-            // For example, check user credentials against the database
-
-            // If authentication is successful
-            bool isAuthenticated = true; // Replace with actual authentication check
-            if (isAuthenticated)
-            {
-                string token = "generated-jwt-token"; // Replace with actual token generation logic
-                return (true, token, null);
-            }
-
-            // If authentication fails
-            return (false, null, "Invalid email or password");
+     
         }
 
         public async Task LogoutAsync()
