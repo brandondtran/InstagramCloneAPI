d99a5b2b34002988c5707786e858b3742b3c468d
Brandon Tran <khoitran00@gmail.com>
2024-08-07 23:10:26 -0700

    WIP

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ builder.Services
     .AddJwtBearer(options =>
     {
         // TODO: User ServiceUrls config
-        options.Authority = "http://localhost:9999/realms/myrealm";
+        options.Authority = "http://localhost:9999/realms/general";
         // TODO: Need to set up proper aud
         options.Audience = "account";
         options.RequireHttpsMetadata = false;
@@ -59,7 +59,7 @@ builder.Services
             ValidateAudience = true,
             ValidateLifetime = true,
             // TODO: User ServiceUrls config
-            ValidIssuer = "http://localhost:9999/realms/myrealm",
+            ValidIssuer = "http://localhost:9999/realms/general",
             // TODO: Need to set up proper aud
             ValidAudience = "account"
         };
---
+++ init-kc-db.sql
@@ -0,0 +1,42 @@
+﻿-- Check if the database exists and create it if it does not
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database
+      WHERE datname = 'keycloak'
+   ) THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE keycloak');
+END IF;
+END
+$do$;
+
+-- Connect to the keycloak database
+\c keycloak
+
+-- Check if the user exists and create it if it does not
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_roles
+      WHERE rolname = 'admin'
+   ) THEN
+CREATE ROLE admin LOGIN PASSWORD 'ZNL%fkv0Ox9X';
+END IF;
+END
+$do$;
+
+-- Check if the user exists in the keycloak database and grant privileges if it does not
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_user
+      WHERE usename = 'admin'
+   ) THEN
+      CREATE USER admin;
+      GRANT ALL PRIVILEGES ON DATABASE keycloak TO admin;
+END IF;
+END
+$do$;
